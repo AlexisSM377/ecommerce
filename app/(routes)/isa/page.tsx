7d746dd17de440ffffc30686e3b07cbfd151ddad
AlexisSM377 <al222111377@gmail.com>
2024-10-10 14:59:49 -0600

    se agrego varias rutas,algun que se agrego la informacion sobre la empresa

--- app/(routes)/isa/page.tsx
+++ app/(routes)/isa/page.tsx
@@ -1,7 +1,10 @@
+"use client"
+import InfoIsa from "./components/infoIsa"
+
 const Page = () => {
     return (
-        <div className="flex mx-auto sm:max-w-4xl md:max-w-6xl mt-6 px-4 sm:px-6 lg:px-8">
-            <h1>Isa</h1>
+        <div className="flex mx-auto sm:max-w-4xl md:max-w-4xl mt-6 px-4 sm:px-6 lg:px-8">
+            <InfoIsa />
         </div>
     )
 }
---
+++ types/details.ts
@@ -0,0 +1,11 @@
+export type DetailsType = {
+    id : number
+    title: string
+    description: string
+    mission: string
+    vision: string
+    objectives: string
+    logo: {
+        url: string
+    }
+}
