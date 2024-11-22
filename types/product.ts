34bd8ae0c67a9aa1fce5e6e7650536d95861d8d8
AlexisSM377 <al222111377@gmail.com>
2024-11-21 21:47:33 -0600

    add size

--- types/product.ts
+++ types/product.ts
@@ -1,34 +1,33 @@
 export type ProductType = {
-    id: number
-    productName: string
-    slug: string
-    description: string
-    active: boolean
-    isFeatured: boolean
-    price: number
-    color: string
-    size : {
-        data: {
-            size: string
-            enabled: boolean
-        }[]
+  id: number;
+  productName: string;
+  slug: string;
+  description: string;
+  active: boolean;
+  isFeatured: boolean;
+  price: number;
+  color: string;
+  size: {
+    data: {
+      size: string;
+      enabled: boolean;
+    }[];
+  };
+  stock: number;
+  gender: string;
+  images: [
+    {
+      id: number;
+      url: string;
     }
-    stock: number
-    gender: string
-    images:[
-        {
-            id: number
-            url: string
-        }
-    ]
-    category: {
-        slug: string
-        categoryName: string
-    }
-    brand: {
-        slug: string
-        nameBrand: string
-    }
-
-
-}
+  ];
+  category: {
+    slug: string;
+    categoryName: string;
+  };
+  brand: {
+    slug: string;
+    nameBrand: string;
+  };
+  selectedSize: string | null;
+};
