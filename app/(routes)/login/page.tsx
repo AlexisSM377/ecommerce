34a6954930bfae8e569c0690cfa761fa0496bcd2
AlexisSM377 <al222111377@gmail.com>
2024-11-29 23:04:45 -0600

    update login page to enable redirect after GitHub sign-in

--- app/(routes)/login/page.tsx
+++ app/(routes)/login/page.tsx
@@ -16,6 +16,7 @@ export default function LoginPage() {
                             'use server'
                             await signIn('github', {
                                 redirectTo: '/profile',
+                                redirect: true,
                             })
                         }}
                         className="w-full"
